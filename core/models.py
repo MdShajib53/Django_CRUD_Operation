376aaa77273959f2c2f59e52b7d93debebe52efe
Md Shajib <shajib.cse.53@gmail.com>
2025-05-23 09:03:15 +0600

    Change Table or Model

--- core/models.py
+++ core/models.py
@@ -15,13 +15,7 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
-    dob = models.DateField()
-    registered_time = models.TimeField()
-    location = models.CharField(max_length=100)
-    address = models.TextField()
-    is_active = models.BooleanField(default=True)
     gpa = models.DecimalField(max_digits=4, decimal_places=2)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
